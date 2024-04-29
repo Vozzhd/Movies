f4d4750bb8c0cc6017c103ecf29800ed3432666b
Vozzhd <Vozzhd@yandex.ru>
2024-04-29 13:40:39 +0300

    Начало 15 спринта - файлы созданы, код работает, всё импортировано

---
+++ app/src/main/java/com/example/movies/IMDbApiService.kt
@@ -0,0 +1,10 @@
+package com.example.movies
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface IMDbApiService {
+    @GET("/en/API/SearchMovie/YOUR_API_KEY/{expression}")
+    fun searchMovies(@Path("expression") expression: String): Call<MoviesSearchResponse>
+}
