5f17c193320456c26aedbea39a273c7bb1557f2b
Vozzhd <Vozzhd@yandex.ru>
2024-04-29 18:14:06 +0300

    MVC - второй вариант, завершённый и с контроллерами

--- app/src/main/java/com/example/movies/Creator.kt
+++ app/src/main/java/com/example/movies/Creator.kt
@@ -1,10 +1,14 @@
 package com.example.movies
 
+import android.app.Activity
 import com.example.movies.data.MoviesRepositoryImpl
 import com.example.movies.data.network.RetrofitNetworkClient
 import com.example.movies.domain.api.MoviesInteractor
 import com.example.movies.domain.api.MoviesRepository
 import com.example.movies.domain.impl.MoviesInteractorImpl
+import com.example.movies.presentation.MoviesSearchController
+import com.example.movies.presentation.PosterController
+import com.example.movies.ui.movies.MoviesAdapter
 
 object Creator {
     private fun getMoviesRepository(): MoviesRepository {
@@ -13,5 +17,16 @@ object Creator {
 
     fun provideMoviesInteractor(): MoviesInteractor {
         return MoviesInteractorImpl(getMoviesRepository())
+    }
+
+    fun provideMoviesSearchController(
+        activity: Activity,
+        adapter: MoviesAdapter
+    ): MoviesSearchController {
+        return MoviesSearchController(activity, adapter)
+    }
+
+    fun providePosterController (activity: Activity) : PosterController {
+        return PosterController(activity)
     }
 }
--- app/src/main/java/com/example/movies/ui/movies/MoviesActivity.kt
+++ app/src/main/java/com/example/movies/ui/movies/MoviesActivity.kt
@@ -1,48 +1,18 @@
-package com.example.movies.ui.movies
-
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.View
-import android.widget.EditText
-import android.widget.ProgressBar
-import android.widget.TextView
-import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.movies.Creator
-import com.example.movies.ui.poster.PosterActivity
 import com.example.movies.R
-import com.example.movies.data.dto.MovieDto
-import com.example.movies.data.dto.MoviesSearchResponse
-import com.example.movies.data.network.IMDbApiService
-import com.example.movies.domain.api.MoviesInteractor
-import com.example.movies.domain.models.Movie
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import com.example.movies.ui.movies.MoviesAdapter
+import com.example.movies.ui.poster.PosterActivity
 
 class MoviesActivity : Activity() {
-    private val moviesInteractor = Creator.provideMoviesInteractor()
-    private val imdbBaseUrl = "https://tv-api.com"
 
     companion object {
         private const val CLICK_DEBOUNCE_DELAY = 1000L
-        private const val SEARCH_DEBOUNCE_DELAY = 2000L
     }
-
-    private lateinit var queryInput: EditText
-    private lateinit var placeholderMessage: TextView
-    private lateinit var moviesList: RecyclerView
-    private lateinit var progressBar: ProgressBar
-
-    private val movies = ArrayList<Movie>()
 
     private val adapter = MoviesAdapter {
         if (clickDebounce()) {
@@ -56,99 +26,25 @@ class MoviesActivity : Activity() {
 
     private val handler = Handler(Looper.getMainLooper())
 
-    private val searchRunnable = Runnable { searchRequest() }
+    private val moviesSearchController = Creator.provideMoviesSearchController(this, adapter)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movies)
-
-        placeholderMessage = findViewById(R.id.placeholderMessage)
-        queryInput = findViewById(R.id.queryInput)
-        moviesList = findViewById(R.id.locations)
-        progressBar = findViewById(R.id.progressBar)
-
-        adapter.movies = movies
-
-        moviesList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        moviesList.adapter = adapter
-
-        queryInput.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                searchDebounce()
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-            }
-
-        })
+        moviesSearchController.onCreate()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        handler.removeCallbacks(searchRunnable)
+        moviesSearchController.onDestroy()
     }
 
-    private fun searchRequest() {
-        if (queryInput.text.isNotEmpty()) {
-
-            placeholderMessage.visibility = View.GONE
-            moviesList.visibility = View.GONE
-            progressBar.visibility = View.VISIBLE
-
-            moviesInteractor.searchMovies(
-                queryInput.text.toString(),
-                object : MoviesInteractor.MoviesConsumer {
-                    override fun consume(foundMovies: List<Movie>) {
-                        handler.post {
-                            progressBar.visibility = View.GONE
-                            movies.clear()
-                            movies.addAll(foundMovies)
-                            moviesList.visibility = View.VISIBLE
-                            adapter.notifyDataSetChanged()
-                            if (movies.isEmpty()) {
-                                showMessage(getString(R.string.nothing_found), "")
-                            } else {
-                                hideMessage()
-                            }
-                        }
-                    }
-                })
-        }
-    }
-
-    private fun showMessage(text: String, additionalMessage: String) {
-        if (text.isNotEmpty()) {
-            placeholderMessage.visibility = View.VISIBLE
-            movies.clear()
-            adapter.notifyDataSetChanged()
-            placeholderMessage.text = text
-            if (additionalMessage.isNotEmpty()) {
-                Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_LONG)
-                    .show()
-            }
-        } else {
-            placeholderMessage.visibility = View.GONE
-        }
-    }
-
-    private fun hideMessage() {
-        placeholderMessage.visibility = View.GONE
-    }
-
-    private fun clickDebounce(): Boolean {
+    private fun clickDebounce() : Boolean {
         val current = isClickAllowed
         if (isClickAllowed) {
             isClickAllowed = false
             handler.postDelayed({ isClickAllowed = true }, CLICK_DEBOUNCE_DELAY)
         }
         return current
-    }
-
-    private fun searchDebounce() {
-        handler.removeCallbacks(searchRunnable)
-        handler.postDelayed(searchRunnable, SEARCH_DEBOUNCE_DELAY)
     }
 }
