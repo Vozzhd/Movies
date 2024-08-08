646d94c0e2d323e4cada9b43d50184f1b4e1a51e
Vozzhd <Vozzhd@yandex.ru>
2024-08-08 23:17:33 +0300

    20 спринт, корутины - самостоятельная рработа сделана ОК

--- app/src/main/java/com/example/movies/movie_search/ui/MoviesFragment.kt
+++ app/src/main/java/com/example/movies/movie_search/ui/MoviesFragment.kt
@@ -1,8 +1,6 @@
 package com.example.movies.movie_search.ui
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
@@ -13,6 +11,7 @@ import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -23,6 +22,9 @@ import com.example.movies.movie_search.domain.model.Movie
 import com.example.movies.movie_search.ui.models.MoviesState
 import com.example.movies.movie_search.ui.models.MoviesViewModel
 import com.example.movies.movie_search.ui.presentation.MoviesAdapter
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MoviesFragment : Fragment() {
@@ -51,8 +53,9 @@ class MoviesFragment : Fragment() {
             }
         }
     )
-    private val handler = Handler(Looper.getMainLooper())
     private lateinit var binding: FragmentMoviesBinding
+
+    private var delayJob: Job? = null
 
     private lateinit var queryInput: EditText
     private lateinit var placeholderMessage: TextView
@@ -158,7 +161,10 @@ class MoviesFragment : Fragment() {
         val current = isClickAllowed
         if (isClickAllowed) {
             isClickAllowed = false
-            handler.postDelayed({ isClickAllowed = true }, CLICK_DEBOUNCE_DELAY)
+            delayJob = viewLifecycleOwner.lifecycleScope.launch {
+                delay(CLICK_DEBOUNCE_DELAY)
+                isClickAllowed = true
+            }
         }
         return current
     }
